fcdd08449bc198850466b23fc5fdefeec3bd8552
DmitryTsyrulik <czyrulik97@mail.ru>
2022-07-21 15:25:04 +0300

    l2

---
+++ src/main/java/course/concurrency/m2_async/executors/spring/AsyncClassTest.java
@@ -0,0 +1,31 @@
+package course.concurrency.m2_async.executors.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableAsync
+public class AsyncClassTest {
+
+    @Autowired
+    public ApplicationContext context;
+
+    @EventListener(ApplicationReadyEvent.class)
+    @Async
+    public void actionAfterStartup() {
+        runAsyncTask();
+    }
+
+
+    public void runAsyncTask() {
+        ThreadPoolTaskExecutor executor = (ThreadPoolTaskExecutor) context.getBean("applicationTaskExecutor");
+        System.out.println("Perfect place for breakpoint ThreadName: " + Thread.currentThread().getName()
+                + " Executor: " + executor.getThreadPoolExecutor() + executor.getCorePoolSize());
+    }
+}
